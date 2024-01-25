3fa14a4787fd10363894dce9119b085244ad4c1a
Antonio <33078450+bsooho@users.noreply.github.com>
2024-01-25 16:09:22 +0900

    Update IS25LP256.h

--- IS25LP256.h
+++ IS25LP256.h
@@ -1,36 +1,50 @@
 //#include <arduino.h>
 //#include <SPI.h>
 
-// フラッシュメモリ IS25LP256の利用開始
+// 플래시 메모리 IS25LP256 사용 시작
 void IS25LP256_begin(uint8_t cs);
-// ステータスレジスタ1の値取得
-uint8_t IS25LP256_readStatusReg1(void);
-// ステータスレジスタ2の値取得
-uint8_t IS25LP256_readStatusReg2();
-// JEDEC ID(Manufacture, Memory Type,Capacity)の取得
-void IS25LP256_readManufacturer(uint8_t* d);
-// Unique IDの取得
-void IS25LP256_readUniqieID(uint8_t* d);
-// 書込み等の処理中チェック
-bool IS25LP256_IsBusy(void);
-// パワーダウン指定 
-void IS25LP256_powerDown(void);
-// 書込み許可設定
-void IS25LP256_WriteEnable(void);
-// 書込み禁止設定
-void IS25LP256_WriteDisable(void);
-// データの読み込み
-uint16_t IS25LP256_read(uint32_t addr,uint8_t *buf,uint16_t n);
-// 高速データの読み込み
-uint16_t IS25LP256_fastread(uint32_t addr,uint8_t *buf,uint16_t n);
-// セクタ単位消去
-bool  IS25LP256_eraseSector(uint16_t sect_no, bool flgwait);
-// 64KBブロック単位消去
-bool  IS25LP256_erase64Block(uint16_t blk_no, bool flgwait);
-// 32KBブロック単位消去
-bool  IS25LP256_erase32Block(uint16_t blk_no, bool flgwait);
-// 全領域の消去
-bool  IS25LP256_eraseAll(bool flgwait);
-// データの書き込み
-uint16_t IS25LP256_pageWrite(uint16_t sect_no, uint16_t inaddr, uint8_t* data, uint16_t n);
 
+// 상태 레지스터 1의 값 가져오기
+uint8_t IS25LP256_readStatusReg1(void);
+
+// 상태 레지스터 2의 값 가져오기
+uint8_t IS25LP256_readStatusReg2();
+
+// JEDEC ID(Manufacture, Memory Type,Capacity) 취득
+void IS25LP256_readManufacturer(uint8_t* d);
+
+// Unique ID 취득
+void IS25LP256_readUniqieID(uint8_t* d);
+
+// 쓰기 등 처리 중 확인
+bool IS25LP256_IsBusy(void);
+
+// 파워 다운 지정 
+void IS25LP256_powerDown(void);
+
+// 쓰기 권한 설정
+void IS25LP256_WriteEnable(void);
+
+// 쓰기 금지 설정
+void IS25LP256_WriteDisable(void);
+
+// 데이터 읽기
+uint16_t IS25LP256_read(uint32_t addr,uint8_t *buf,uint16_t n);
+
+// 고속 데이터 읽기
+uint16_t IS25LP256_fastread(uint32_t addr,uint8_t *buf,uint16_t n);
+
+// 섹터 단위 지우기
+bool  IS25LP256_eraseSector(uint16_t sect_no, bool flgwait);
+
+// 64KB 블록 단위 지우기
+bool  IS25LP256_erase64Block(uint16_t blk_no, bool flgwait);
+
+// 32KB 블록 단위 지우기
+bool  IS25LP256_erase32Block(uint16_t blk_no, bool flgwait);
+
+// 전체 영역 지우기
+bool  IS25LP256_eraseAll(bool flgwait);
+
+// 데이터 쓰기
+uint16_t IS25LP256_pageWrite(uint16_t sect_no, uint16_t inaddr, uint8_t* data, uint16_t n);
