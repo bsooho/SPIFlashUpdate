75d9907060d316b1ce323a83bcf382881d624445
Antonio <33078450+bsooho@users.noreply.github.com>
2024-02-07 17:49:58 +0900

    Update main.c

--- main.c
+++ main.c
@@ -112,13 +112,14 @@ int main() {
         return 1;
     }
 
+  /*
     while(1){
         gpiod_line_set_value(line, 0); // Set line low (0V)
         sleep(1);
         gpiod_line_set_value(line, 1); // Set line high (3.3V)
         sleep(1);
     }
-
+*/
   
     gpiod_line_set_value(line, 0); // Set line low (0V)
     printf("SPI Bypass Disabled!\n\n");
