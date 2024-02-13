e9c412e5964d5e04980f033c60c6b168de029039
Antonio <33078450+bsooho@users.noreply.github.com>
2024-02-13 20:23:32 +0900

    Update main.c

--- main.c
+++ main.c
@@ -274,6 +274,15 @@ int main() {
     ssize_t read_bytes;
     uint32_t flash_address = 0x0; // Start address in SPI Flash where data will be written
     uint16_t int_addr;  //internal address within a sector (total 4096byte)
+
+  
+  read_bytes = fread(sector_buf, 1, SECTOR_SIZE, binaryFile)
+  for (int j = 0; j < CHUNK_SIZE; j++) {
+    buf[j] = sector_buf[int_addr+j];
+  }
+  dump(buf,256);
+  return 0;
+
   
     while ((read_bytes = fread(sector_buf, 1, SECTOR_SIZE, binaryFile)) > 0) {
       int_addr=0;  //initialize int_addr
