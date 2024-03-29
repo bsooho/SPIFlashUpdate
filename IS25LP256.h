e5726b78967fc14f6f1a56b1b662445262f7786a
Antonio <33078450+bsooho@users.noreply.github.com>
2024-03-29 16:34:44 +0900

    Update IS25LP256.h

--- IS25LP256.h
+++ IS25LP256.h
@@ -1,47 +1,48 @@
 //#include <arduino.h>
 //#include <SPI.h>
 
-// 플래시 메모리 IS25LP256 사용 시작
+// Begin of flash memory operation by specify SPI channel number.
+// For example 0, 1, or 2, usually 0 is used.
 void IS25LP256_begin(uint8_t cs);
 
-// 상태 레지스터의 값 가져오기
+// Read status register
 uint8_t IS25LP256_readStatusReg(void);
 
-// JEDEC ID(Manufacture, Memory Type,Capacity) 취득
+// Read JEDEC ID(Manufacture, Memory Type,Capacity)
 void IS25LP256_readManufacturer(uint8_t* d);
 
-// Unique ID 취득
+// Read Unique ID of the memory
 void IS25LP256_readUniqieID(uint8_t* d);
 
-// 쓰기 등 처리 중 확인
+// Check if memory is uder operation, used during write process
 bool IS25LP256_IsBusy(void);
 
-// 파워 다운 지정 
+// Set power down 
 void IS25LP256_powerDown(void);
 
-// 쓰기 권한 설정
+// Set write enable
 void IS25LP256_WriteEnable(void);
 
-// 쓰기 금지 설정
+// Set write disable
 void IS25LP256_WriteDisable(void);
 
-// 데이터 읽기
+// Read data
 uint16_t IS25LP256_read(uint32_t addr,uint8_t *buf,uint16_t n);
 
-// 고속 데이터 읽기
+// Fast read data
 uint16_t IS25LP256_fastread(uint32_t addr,uint8_t *buf,uint16_t n);
 
-// 섹터 단위 지우기
+// Erase by sector
 bool  IS25LP256_eraseSector(uint16_t sect_no, bool flgwait);
 
-// 64KB 블록 단위 지우기
+// Erase by 64KB block
 bool  IS25LP256_erase64Block(uint16_t blk64_no, bool flgwait);
 
-// 32KB 블록 단위 지우기
+// Erase by 32KB block
 bool  IS25LP256_erase32Block(uint16_t blk32_no, bool flgwait);
 
-// 전체 영역 지우기
+// Erase all (Entire of memory to '1')
 bool  IS25LP256_eraseAll(bool flgwait);
 
-// 데이터 쓰기
+// Write data
 uint16_t IS25LP256_pageWrite(uint16_t sect_no, uint16_t inaddr, uint8_t* data, uint16_t n);
